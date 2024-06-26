94deed6f1761a8d4cadd822b3928521f303deb54
Tejas Ulawekar <tejasau21hcompe@student.mes.ac.in>
2024-06-26 17:13:22 +0530

    Add files via upload

---
+++ Online_cab/about_project.php
@@ -0,0 +1,44 @@
+<?php
+  include('db/connection.php');
+  include('include/header.php');
+ ?>
+<html>
+ <head>
+     <title> About Project </title>
+     <link rel="stylesheet" href="css/about_project.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+ </head>
+ <body >
+<div class="container1">
+      <ul class="navbar">
+        <li><a href="home.php">Home</a></li>
+    <li><a href="about_project.php">About Project</a></li>
+    <li><a href="register.php">Register</a></li>
+      <li><a href="login.php"> Login</a></li>
+      <li><a href="contactus.php">Contact Us</a></li>
+     </ul>
+    </div>
+<div class="rowbar">
+  <div class="container">
+    <div class="col-md-9">
+      <h2 class="hd">About TRAVELDUO</h2>
+      <p class="desc">
+        <hr>
+        TRAVELDUO is an online two-wheeler ride booking system which helps the drivers and passengers. Bike drivers are unable to find the passengers every time and similarly passengers are also unable to find cabs at the necessity time. People can easily find the bike rides and so bike drivers to find their passengers through online cab system.
+        <br> <br>
+        This website is for the online cart-bike booking (2-wheeled vehicles).
+        This website will have one signup page for bike taxi drivers where the bike taxi drivers can be added,
+        can see recent information’s like who is picked by whom, what’s the notifications (receives location) of the receiver
+        (the people you are going to pick up).and active status (Like how many cart-bike drivers are available now and who are
+        available too).
+    </div>
+    <div class="col-md-3" style="margin-left: 0px;">
+
+      <img src="img/img2.jpg" class="ad1" alt="img" style="margin-top:48px;">
+
+    </div>
+  </div>
+</div>
+</body>
+</html>
---
+++ Online_cab/include/header.php
@@ -0,0 +1,6 @@
+
+
+<div class="row topnav">
+
+         <h3 style="margin-left: 40%; margin-top:23px;">TRAVELDUO</h3>
+</div>
